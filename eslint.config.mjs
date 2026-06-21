484f075f6760d284fb02303bcbdb2b3dada65d47
Denis Lapshin <denis@lapsha.dev>
2026-06-22 02:33:48 +0400

    Setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,162 +1,167 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default withNuxt({
+export default withNuxt(
+  {
     rules: {
+      /*
+       * =====================================================
+       * JavaScript
+       * =====================================================
+       */
 
-        /*
-         * =====================================================
-         * JavaScript
-         * =====================================================
-         */
+      // '
+      quotes: ['error', 'single'],
 
-        // '
-        quotes: ['error', 'single'],
+      // ;
+      semi: ['error', 'always'],
 
-        // ;
-        semi: ['error', 'always'],
+      // trailing comma
+      'comma-dangle': ['error', 'always-multiline'],
 
-        // trailing comma
-        'comma-dangle': ['error', 'always-multiline'],
+      // 120 chars
+      'max-len': [
+        'warn',
+        {
+          code: 120,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
 
-        // 120 chars
-        'max-len': [
-            'warn',
-            {
-                code: 120,
-                ignoreUrls: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-                ignoreComments: true,
-                ignoreTrailingComments: true,
-                ignoreRegExpLiterals: true,
-            },
-        ],
+      // console.warn/error only
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
 
-        // console.warn/error only
-        'no-console': [
-            'error',
-            {
-                allow: ['warn', 'error'],
-            },
-        ],
+      // debugger forbidden
+      'no-debugger': 'error',
 
-        // debugger forbidden
-        'no-debugger': 'error',
+      // no var
+      'no-var': 'error',
 
-        // no var
-        'no-var': 'error',
+      // prefer const
+      'prefer-const': 'error',
 
-        // prefer const
-        'prefer-const': 'error',
+      // === only
+      eqeqeq: ['error', 'always'],
 
-        // === only
-        eqeqeq: ['error', 'always'],
+      // { foo }
+      'object-shorthand': ['error', 'always'],
 
-        // { foo }
-        'object-shorthand': ['error', 'always'],
+      // `hello ${name}`
+      'prefer-template': 'error',
 
-        // `hello ${name}`
-        'prefer-template': 'error',
+      // always braces
+      curly: ['error', 'all'],
 
-        // always braces
-        curly: ['error', 'all'],
+      // ()
+      'arrow-body-style': ['error', 'as-needed'],
 
-        // ()
-        'arrow-body-style': ['error', 'as-needed'],
+      // no multiple statements
+      'max-statements-per-line': [
+        'error',
+        {
+          max: 1,
+        },
+      ],
 
-        // no multiple statements
-        'max-statements-per-line': [
-            'error',
-            {
-                max: 1,
-            },
-        ],
+      // duplicate imports
+      'no-duplicate-imports': 'error',
 
-        // duplicate imports
-        'no-duplicate-imports': 'error',
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+        },
+      ],
 
-        'no-multiple-empty-lines': [
-            'error',
-            {
-                max: 1,
-            },
-        ],
+      /*
+       * =====================================================
+       * TypeScript
+       * =====================================================
+       */
 
-        /*
-         * =====================================================
-         * TypeScript
-         * =====================================================
-         */
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
 
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
-                caughtErrorsIgnorePattern: '^_',
-                destructuredArrayIgnorePattern: '^_',
-            },
-        ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false,
+        },
+      ],
 
-        '@typescript-eslint/consistent-type-imports': ['error', {
-            prefer: 'type-imports',
-            disallowTypeAnnotations: false,
-        }],
+      '@typescript-eslint/no-explicit-any': 'warn',
 
-        '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'error',
 
-        '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-empty-function': [
+        'error',
+        {
+          allow: ['arrowFunctions'],
+        },
+      ],
 
-        '@typescript-eslint/no-empty-function': [
-            'error',
-            {
-                allow: ['arrowFunctions'],
-            },
-        ],
+      // TS can infer types
+      '@typescript-eslint/explicit-function-return-type': 'off',
 
-        // TS can infer types
-        '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-magic-numbers': [
+        'warn',
+        {
+          ignore: [-1, 0, 1, 2, 1000],
+          ignoreEnums: true,
+          ignoreArrayIndexes: true,
+          ignoreDefaultValues: true,
+          ignoreClassFieldInitialValues: true,
+          ignoreReadonlyClassProperties: true,
+        },
+      ],
 
-        '@typescript-eslint/no-magic-numbers': [
-            'warn',
-            {
-                ignore: [-1, 0, 1, 2, 1000],
-                ignoreEnums: true,
-                ignoreArrayIndexes: true,
-                ignoreDefaultValues: true,
-                ignoreClassFieldInitialValues: true,
-                ignoreReadonlyClassProperties: true,
-            },
-        ],
+      /*
+       * =====================================================
+       * Vue
+       * =====================================================
+       */
 
-        /*
-         * =====================================================
-         * Vue
-         * =====================================================
-         */
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always',
+          },
 
-        'vue/html-self-closing': [
-            'error',
-            {
-                html: {
-                    void: 'always',
-                    normal: 'always',
-                    component: 'always',
-                },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
 
-                svg: 'always',
-                math: 'always',
-            },
-        ],
-
-        'vue/attributes-order': 'error',
+      'vue/attributes-order': 'error',
     },
 
     files: ['**/*.ts', '**/*.vue', '**/*.tsx', '**/*.jsx'],
     languageOptions: {
-        parserOptions: {
-            project: './tsconfig.json',
-        },
+      parserOptions: {
+        project: './.nuxt/tsconfig.app.json',
+      },
     },
-
-});
+  },
+  eslintConfigPrettier,
+);
